96bd7336168f120de0c90fee0e79729502bdfa93
Livia <liviaishak1@gmail.com>
2018-04-07 12:04:14 -0700

    attempting to fix callback #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
-  def downgrade
-      @wikis = current_user.wikis
+  current_user.downgrade
 
-      @wikis.unscoped.update_all(private: 'false')
-      current_user.update_attribute(:role, 'standard')
+  flash[:notice] = "#{current_user.email} you're account has been downgraded"
+  redirect_to edit_user_registration_path
 
-      flash[:notice] = "#{current_user.email} you're account has been downgraded"
-      redirect_to edit_user_registration_path
-  end
 end
+
+# def downgrade
+#     @wikis = current_user.wikis
+#
+#     @wikis.unscoped.update_all(private: 'false')
+#     current_user.update_attribute(:role, 'standard')
+#
+#     flash[:notice] = "#{current_user.email} you're account has been downgraded"
+#     redirect_to edit_user_registration_path
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,14 @@ class User < ApplicationRecord
     wikis.update_all(private: false)
   end
 
+  # def downgrade
+  #     @wikis = current_user.wikis
+  #
+  #     @wikis.unscoped.update_all(private: 'false')
+  #     current_user.update_attribute(:role, 'standard')
+  #
+  #     flash[:notice] = "#{current_user.email} you're account has been downgraded"
+  #     redirect_to edit_user_registration_path
+  # end
+
 end
